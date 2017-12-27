c0e435d077e6c4f8d5368cedfa4ec6d889c279b4
Liuxyly <cenguor@gmail.com>
2017-12-27 17:48:31 +0800

    Project init
    Signed-off-by: yu.liu <yu.liu@xsintech.com>

---
+++ src/main/java/com/silence/ssm/quartz/schedule/TaskJob.java
@@ -0,0 +1,16 @@
+package com.silence.ssm.quartz.schedule;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TaskJob {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TaskJob.class);
+	
+	public void run(){
+		LOGGER.info("this is quartz schedule,the task Job is running! today is " + new Date());
+		LOGGER.info("this is test information");
+	}
+}
---
+++ src/main/scala/com/silence/ssm/scala/controller/Test1.scala
@@ -0,0 +1,52 @@
+package com.silence.ssm.scala.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import com.wordnik.swagger.annotations.Api
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RequestParam
+import com.wordnik.swagger.annotations.ApiOperation
+import com.silence.ssm.scala.service.ScalaTestService
+import com.wordnik.swagger.annotations.ApiParam
+import org.springframework.beans.factory.annotation.Autowired
+import com.silence.ssm.service.UserService
+
+@Api(value = "ScalaController", description = "测试scala")
+@Controller
+@RequestMapping(value=Array {"/scala"})
+class Test1 @Autowired()(scalaTestService:ScalaTestService,userService:UserService){
+  
+  @ResponseBody
+  @ApiOperation(value = "Scala实现单词统计", httpMethod = "POST", notes = "scalaWordCount")
+  @RequestMapping(value=Array{"/scalaWordCount"},method=Array{RequestMethod.POST})
+  def scalaWorkCount(@ApiParam(required = true, name = "line", value = "给定的字符串") @RequestParam(value = "line") line:String,
+      @ApiParam(required = true, name = "pattern", value = "分隔符") @RequestParam(value = "pattern") pattern:String):String = {
+    scalaTestService.wordcount(line, pattern)
+  }
+  
+  @ResponseBody
+  @ApiOperation(value = "测试Scala2", httpMethod = "POST", notes = "testScala2")
+  @RequestMapping(value=Array{"/test2"},method=Array{RequestMethod.POST})
+  def test2(@ApiParam(required = true, name = "name", value = "用户姓名") @RequestParam(value = "name") name:String):String = {
+     println(scalaTestService.execute("value"))
+    "this is scala controller test"
+  }
+  
+  @ResponseBody
+  @ApiOperation(value = "测试Scala3", httpMethod = "DELETE", notes = "testScala3")
+  @RequestMapping(value=Array{"/test3"},method=Array{RequestMethod.DELETE})
+  def test3(@ApiParam(required = true, name = "id", value = "用户id") @RequestParam(value = "id") id:Int):String = {
+     println(scalaTestService.execute(id+""))
+    "this is scala controller test"
+  }
+   
+  @ResponseBody
+  @ApiOperation(value = "scala userService", httpMethod = "GET", notes = "test userService in scala")
+  @RequestMapping(value=Array{"/getUserById"},method=Array{RequestMethod.GET})
+  def getUserById(@ApiParam(required = true, name = "id", value = "用户id") @RequestParam(value = "id") id:Int):String = {
+     println(scalaTestService.execute(id+""))
+    "this is scala controller test"
+  }
+    
+}
